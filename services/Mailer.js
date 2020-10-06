b52163586f6aebf1c63904fbed9611ea234079a9
Jérémie <franchomme@ip-10-251-250-102.eu-west-1.compute.internal>
2020-10-06 19:50:16 +0200

    from scratch

--- services/Mailer.js
+++ services/Mailer.js
@@ -56,21 +56,3 @@ module.exports = Mailer;
 
 
 this.from_email = new helper.Email('jeremie.franchomme@icloud.com');
-
-// const sgMail = require("@sendgrid/mail");
-// sgMail.setApiKey('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');
-// const msg = {
-//   to: "jeremie.franchomme@gmail.com",
-//   from: "jeremie.franchomme@icloud.com",
-//   subject: "Test",
-//   text: "and easy to do anywhere, even with Node.js",
-//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-// };
-// sgMail
-//   .send(msg)
-//   .then(() => {
-//     console.log('Email sent')
-//   })
-//   .catch((error) => {
-//     console.error(error)
-//   })
